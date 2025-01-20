f8922337e30940328e85e9912df389e7ad5d9d48
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-20 09:37:38 +0530

    modified

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const app = express();
 const fileupload = require("express-fileupload")
 
 require("dotenv").config()
-require("./config/database").connectDatabse();
-require("./config/cloudinary").clodinaryConnect();
-const userRoutes = require("./routes/dataroutes")
+const connectDatabase = require('./config/database.jsx');
+connectDatabase();
+const cloud = require("./config/cloudinary.jsx");
+cloud.cloudinaryConnect();
+const userRoutes = require("./routes/dataroutes.jsx")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(fileupload())
+app.use(fileupload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/'
+}));
 app.use('/api/v1/upload',userRoutes)
 
 const PORT = process.env.PORT || 3000
