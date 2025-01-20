7e9319d714cc8bf2de16a6b52dd5233a2e5d23fe
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-20 14:14:34 +0530

    changes made

--- config/cloudinary.jsx
+++ config/cloudinary.jsx
@@ -1,6 +1,6 @@
 const cloudinary = require("cloudinary").v2
 
-exports.clodinaryConnect = ()=>{
+exports.cloudinaryConnect = ()=>{
     try{
         cloudinary.config({
             cloud_name: process.env.CLOUDINARY_NAME,
